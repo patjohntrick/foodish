c8a4a71b874c229593dc96f5b125cfa57ce1b86f
patrickkk <patrickjohn.resurreccion18@gmail.com>
2021-08-25 15:01:28 +0800

    add media queries in 768px

--- src/components/Fourthsection.js
+++ src/components/Fourthsection.js
@@ -18,9 +18,11 @@ function Fourthsection() {
                 <h1>Wedding Event</h1>
                 <p>Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 <Blckbtn />
-                <h4 className="ftext text">family gathering</h4>
-                <h4 className="stext text">friends bonding</h4>
-                <h4 className="ttext text">wedding event</h4>
+                <div className="category">
+                    <h4 className="ftext text">family gathering</h4>
+                    <h4 className="stext text">friends bonding</h4>
+                    <h4 className="ttext text">wedding event</h4>
+                </div>
             </div>
         </section>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../App.css';
 
 const Navbar = () => {
-    const linkItem = ['Home', 'Menu', 'About', 'Contact us']
+    const linkItem = ['Home', 'Menu', 'About', 'Contact us'];
+    const [navBar , setnavBar] = useState(false);
+    function handleChange(){
+        setnavBar(!navBar);
+    }
     return (
         <nav>
+            <i className="fas fa-bars nav-bar" onClick={handleChange}></i>
             <div className="logo">
                 <h1>.foodish</h1>
             </div>
-            <ul className="nav-links">
+            <ul className={navBar ? "nav-links" : "nav-links show-nav"}>
+            <i className="fas fa-times nav-ex" onClick={handleChange}></i>
                 {linkItem.map((item) => <li id={item}>{item}</li>)}
             </ul>
         </nav>
